42aab37c6683029ccb8111457858d716b855b062
Nataliapine <nataliapine@icloud.com>
2021-08-10 12:28:44 +0100

    added new task in js file, changed list design in css and html files

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
 
 //Criar uma nova tarefa quando clicar no botao +
 function addNewTask() {
     let newTask = document.createElement("li");
     let inputValue = document.getElementById("taskName").value;
     let text = document.createTextNode(inputValue);
+    let erro = document.getElementById("error")
     newTask.appendChild(text);
 
     if(inputValue === '') {
-        alert('You must write something!')
+      erro.textContent = "Please, write a task name."
     }else {
         document.getElementById("myList").appendChild(newTask);
     }
     document.getElementById("taskName").value = "";
-
-    let buttonDelete = document.createElement("BUTTON");
-    let buttonIcon =  document.createTextNode("\u00D7");
-    buttonDelete.className = "close";
-    buttonDelete.appendChild(buttonIcon);
-    newTask.appendChild(buttonDelete);
-
-    // let span = document.createElement("SPAN");
-    // // let icon = document.getElementById("iconButtonDelete").value;
-    // let icon = document.getElementsByClassName("fas fa-trash-alt").value;
-    // let button =  document.createTextNode(icon);
-    // span.className = "close";
-    // span.appendChild(button);
-    // newTask.appendChild(span);
+   
+    let checkboxElement = document.createElement("INPUT");
+    checkboxElement.setAttribute("type", "checkbox")
+    let list = document.createElement("li");
+    let linkElement = document.createElement("i");
     
-
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          let div = this.parentElement;
-          div.style.display = "none";
-        }
-      }
+    linkElement.setAttribute('class', 'fas fa-trash-alt')
+    linkElement.className = "fas fa-trash-alt";
+    linkElement.appendChild(checkboxElement);
+    linkElement.appendChild(list);
+    newTask.appendChild(linkElement);
+   
 };
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-let i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+
 
