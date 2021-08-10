e8bd8296a1660505525a8ff84cb0fa3e42a55c79
Nataliapine <nataliapine@icloud.com>
2021-08-10 17:46:32 +0100

    code refactor ( add new tas)

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
-
-//Criar uma nova tarefa quando clicar no botao +
 function addNewTask() {
-    let newTask = document.createElement("li");
-    let inputValue = document.getElementById("taskName").value;
-    let text = document.createTextNode(inputValue);
-    let erro = document.getElementById("error")
-    newTask.appendChild(text);
+  const erro = document.getElementById("error");
+  const taskName = document.getElementById("taskName").value; 
+  const original = document.getElementById("newElement");
+  const clone = original.cloneNode(true);
 
-    if(inputValue === '') {
-      erro.textContent = "Please, write a task name."
-    }else {
-        document.getElementById("myList").appendChild(newTask);
-    }
-    document.getElementById("taskName").value = "";
-   
-    let checkboxElement = document.createElement("INPUT");
-    checkboxElement.setAttribute("type", "checkbox")
-    let list = document.createElement("li");
-    let linkElement = document.createElement("i");
-    
-    linkElement.setAttribute('class', 'fas fa-trash-alt')
-    linkElement.className = "fas fa-trash-alt";
-    linkElement.appendChild(checkboxElement);
-    linkElement.appendChild(list);
-    newTask.appendChild(linkElement);
-   
-};
+  if(taskName === '') {
+    erro.textContent = "Please, write a task name."
+    return;
+  };
 
-
+  clone.removeAttribute("id");
+  clone.getElementsByTagName("span")[0].innerHTML = taskName; 
+  document.getElementById("myList").appendChild(clone);
+  document.getElementById("taskName").value = ""; 
+}
 
