5a6ef95f2d953bcc9444e3b6854025e805397404
Nataliapine <nataliapine@icloud.com>
2021-08-10 18:31:40 +0100

    added task on enter

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function addNewTask() {
+function addNewTask(e) {
+  console.log('e: ', e);
+  debugger;
+  e.preventDefault();
+
   const erro = document.getElementById("error");
   const taskName = document.getElementById("taskName").value; 
   const original = document.getElementById("newElement");
@@ -13,6 +17,8 @@ function addNewTask() {
   clone.getElementsByTagName("span")[0].innerHTML = taskName; 
   document.getElementById("myList").appendChild(clone);
   document.getElementById("taskName").value = ""; 
+
+  // return false;
 }
 
 function removeTask(icon) {
